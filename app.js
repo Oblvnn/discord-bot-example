ad38b9926478144c8110bd4af27b28db0a383bd8
Oblivion サン <OblivionSan@users.noreply.github.com>
2017-10-08 00:01:07 +0100

    Added args and some moderation commands.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //Make sure to install discord.js using NPM.
 const client = new Discord.Client();
 
-const config = require('./config.json'); //Gets config.json
+const config = require('./config.json'); //Gets config.json.
 const prefix = config.prefix; //Reads prefix line in config.json.
 
 function cmd(str, msg) { //Function for commands.
@@ -16,9 +16,10 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.author.bot) return; //If author is a bot returns nothing.
 	if (message.channel.type === 'dm') return; //Disables commands in dms.
+	const args = message.content.split(/[ ]+/); //Defines args.
 
 	if (cmd('ping', message)) {
-		message.channel.send(`🏓 Pong! - API Lantancy: **${Date.now() - message.createdTimestamp}**ms`);
+		message.channel.send(`API Lantancy: **${Date.now() - message.createdTimestamp}**ms`);
 	}
 
 	if (cmd('hex', message)) {
@@ -27,6 +28,42 @@ client.on('message', message => {
 			.setTitle(`#${color}`)
 			.setColor(`#${color}`);
 		message.channel.send({ embed: embed });
+	}
+
+	if (cmd("kick", message)) {
+		if (!message.member.permissions.has("KICK_MEMBERS")) //Checks if user has permissions.
+			return message.channel.send(`⚠️ **${message.author.username}**, You don't have the permissions to kick users.`);
+		if (message.member.permissions.has("KICK_MEMBERS")) {
+			var member = message.mentions.members.first();
+			if (!member)
+				return message.channel.send(`⚠️ **${message.author.username}**, Please mention a valid user of this server.`);
+			if (!member.kickable)
+				return message.channel.send(`⚠️ **${message.author.username}**, You can't kick **${member.user.tag}**.`);
+			message.guild.member(message.mentions.users.first()).kick(); //Performs the kick on given user.
+			message.channel.send(`✅ **${message.author.username}** has kicked **${member.user.tag}**`);
+		}
+	}
+
+	if (cmd("ban", message)) {
+		if (!message.member.permissions.has("BAN_MEMBERS")) //Checks if user has permissions.
+			return message.channel.send(`⚠️ **${message.author.username}**, You don't have the permissions to ban users.`);
+		if (message.member.permissions.has("BAN_MEMBERS")) {
+			let member = message.mentions.members.first();
+			if (!member)
+				return message.channel.send(`⚠️ **${message.author.username}**, Please mention a valid user of this server.`);
+			if (!member.bannable)
+				return message.channel.send(`⚠️ **${message.author.username}**, You can't ban **${member.user.tag}**.`);
+			message.guild.member(message.mentions.users.first()).ban(); //Performs the ban on given user.
+			message.channel.send(`✅ **${message.author.username}** has banned **${member.user.tag}**`);
+		}
+	}
+
+	if (cmd("prune", message)) {
+		const deleteCount = parseInt(args[0], 10);
+		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+			return message.channel.send(`⚠️ **${message.author.username}**, please provide a number between 2 and 100 for the number of messages to delete`);
+		const fetched = message.channel.fetchMessages({ count: deleteCount });
+		message.channel.bulkDelete(fetched).catch(error => message.channel.send(`⚠️ **${message.author.username}**, couldn't delete messages because of: ${error}`));
 	}
 
 });
