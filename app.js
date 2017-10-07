69c51dc8698d6fdf7f9118da33b9c79c385b1546
Oblivion サン <OblivionSan@users.noreply.github.com>
2017-10-07 18:02:38 +0100

    Added a ready event.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const Discord = require('discord.js'); //Make sure to install discord.js using NPM.
 const client = new Discord.Client();
 
-const config - require('./config.json');
+const config = require('./config.json'); //Gets config.json
 
 const prefix = config.prefix; //Reads prefix line in config.json.
 
 function cmd(str, msg) { //Function for commands.
     return msg.content.startsWith(prefix + str);
 }
+client.on('ready', () => {
+	console.log(`[Bot is online | Node: ${process.version} | Discord.js: ${Discord.version}]\nConnected as: ${client.user.username} (ID: ${client.user.id})\nGuilds Connected: ${client.guilds.size}`);
+	client.user.setGame(`${config.prefix}help | ${client.guilds.size} Servers`);
+});
 
 client.on('message', message => {
+    if (message.author.bot) return; //If author is a bot returns nothing.
+	if (message.channel.type === 'dm') return; //Disables commands in dms.
 
     if (cmd('hex', message)) {
       let color = ((1 << 24) * Math.random() | 0).toString(16); //Generates random hex value.
